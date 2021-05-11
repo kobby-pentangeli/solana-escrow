75765d490264caba5b8af3748f3fda01a090933f
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-05-11 08:46:12 +0000

    feat: Initial working implementation of the escrow program

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Solana Escrow
+
 An escrow program (smart contract) built for the Solana blockchain
+
+## Environment Setup
+
+1. Install Rust from <https://www.rust-lang.org/tools/install>
+2. Install Solana from <https://docs.solana.com/cli/install-solana-cli-tools>
+
+### Build and test the program compiled for BPF
+
+```rust
+cargo build-bpf
+cargo test-bpf
+```
---
+++ src/error.rs
@@ -0,0 +1,25 @@
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    /// Not Rent Exempt
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+    /// Expected Amount Mismatch
+    #[error("Expected Amount Mismatch")]
+    ExpectedAmountMismatch,
+    /// Amount Overflow
+    #[error("Amount Overflow")]
+    AmountOverflow,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
